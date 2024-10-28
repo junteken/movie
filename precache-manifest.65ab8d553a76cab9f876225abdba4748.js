c9f853c77f0d8e0cbb49af2ea2b874940a7ed60c
beanzsoft <junteken@gmail.com>
2024-10-28 17:26:37 +0900

    Updates

---
+++ precache-manifest.65ab8d553a76cab9f876225abdba4748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b47382272592fc04414a6cf2f98221",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "10529487406fb8f46619",
+    "url": "/movie/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7dd5cc8fa71b37ee2481",
+    "url": "/movie/static/js/2.fb1b8c56.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/movie/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "10529487406fb8f46619",
+    "url": "/movie/static/js/main.922737b1.chunk.js"
+  },
+  {
+    "revision": "a32e606e12959514de2f",
+    "url": "/movie/static/js/runtime~main.4a0f5908.js"
+  }
+]);
